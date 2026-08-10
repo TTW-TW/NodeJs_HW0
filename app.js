85528f981b5ae24b23106b1abcdc764eb9f09e55
TTW-TW <forestsound520@gmail.com>
2026-08-11 01:29:39 +0800

    update package, delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-var http = require("http");
-http.createServer(function (request, response) {
-    console.log(request);
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write("hello~~~");
-    response.end();
-}).listen(8080);
